6d70b3c4b04521d17a04a713fa7710a059287902
Timo Michna <timomichna@yahoo.de>
2021-10-09 05:52:05 +0200

    Add symfony bundle skeleton

---
+++ instrumentation/symfony/src/OpenTelemetryBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace OpenTelemetry\Instrumentation\Symfony\OpenTelemetryBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+    class OpenTelemetryBundle extends Bundle
+{
+    public function build(ContainerBuilder $container): void
+    {
+
+    }
+}
